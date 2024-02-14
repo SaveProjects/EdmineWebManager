ce38150207cac412b72d7287dbaaccd821a13a00
Milo <milovtt@gmail.com>
2024-02-14 14:02:00 +0100

    Login back

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route pour afficher le formulaire de connexion
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+
+// Route pour traiter les données du formulaire de connexion
+Route::post('/login', [AuthController::class, 'login']);
+
